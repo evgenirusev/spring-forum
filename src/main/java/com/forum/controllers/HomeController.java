f994dfadca59de5f0c92efcc14beedda2b6a5ea1
rusev9 <evg.rusev@gmail.com>
2018-11-20 23:07:06 +0200

    update front-end

--- src/main/java/com/forum/controllers/HomeController.java
+++ src/main/java/com/forum/controllers/HomeController.java
@@ -1,14 +1,16 @@
 package com.forum.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = {"","/", "/home"})
-    public String index() {
+    @RequestMapping(value = {"", "/", "/home"})
+    public String index(Model model) {
+        model.addAttribute("viewName", "views/index");
         return "layout";
     }
 }
