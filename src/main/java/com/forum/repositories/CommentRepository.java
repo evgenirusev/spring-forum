9e5e11e7157ec1bfe86d1540c59f8931928fd18f
rusev9 <evg.rusev@gmail.com>
2018-11-23 20:02:12 +0200

    add CommentRepository/CommentService

---
+++ src/main/java/com/forum/repositories/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.forum.repositories;
+
+import com.forum.entities.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
--- src/main/java/com/forum/services/CommentService.java
+++ src/main/java/com/forum/services/CommentService.java
@@ -1,4 +1,10 @@
 package com.forum.services;
 
+import com.forum.dtos.comments.CommentDto;
+
+import java.util.List;
+
 public interface CommentService {
+    public List<CommentDto> findById(Long id);
+
 }
