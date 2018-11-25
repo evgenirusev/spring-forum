6be853833d0aa4efeb5861e661a711819047aad5
rusev9 <evg.rusev@gmail.com>
2018-11-25 21:08:59 +0200

    UserRegisterDto

--- src/main/java/com/forum/controllers/UserController.java
+++ src/main/java/com/forum/controllers/UserController.java
@@ -1,13 +1,23 @@
 package com.forum.controllers;
 
+import com.forum.dtos.users.UserRegisterDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController extends BaseController {
+
     @GetMapping("/register")
     public ModelAndView register() {
         return super.view("views/users/register", "Register");
+    }
+
+    @PostMapping("register")
+    public ModelAndView registerConfirm(@ModelAttribute UserRegisterDto userRegisterDto) {
+        // TODO: implement
+        return null;
     }
 }
---
+++ src/main/java/com/forum/dtos/users/UserRegisterDto.java
@@ -0,0 +1,46 @@
+package com.forum.dtos.users;
+
+public class UserRegisterDto {
+    private String username;
+
+    private String password;
+
+    private String confirmPassword;
+
+    private String email;
+
+    public UserRegisterDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
