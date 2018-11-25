bcc16509e80180ea623dc5fa0241b61a9af9f3b1
rusev9 <evg.rusev@gmail.com>
2018-11-25 18:15:55 +0200

    Abstract BaseController, polish front-end

---
+++ src/main/java/com/forum/controllers/BaseController.java
@@ -0,0 +1,33 @@
+package com.forum.controllers;
+
+import org.springframework.web.servlet.ModelAndView;
+
+abstract class BaseController {
+    private static final String LAYOUT_VIEW_NAME = "layout";
+    private static final String DEFAULT_TITLE_NAME = "Spring-Answers";
+    private static final String DEFAULT_THYMELEAF_VIEW_NAME_KEY = "viewName";
+    private static final String DEFAULT_THYMELEAF_TITLE_KEY = "title";
+    private static final String DEFAULT_SPRING_REDIRECT_KW = "redirect:";
+
+    ModelAndView view(String view){
+        return this.view(view, null);
+    }
+
+    ModelAndView view(String view, String title){
+        return this.view(view, title);
+    }
+
+    ModelAndView view(String view, String title, ModelAndView modelAndView) {
+        modelAndView.addObject(DEFAULT_THYMELEAF_VIEW_NAME_KEY, view);
+        modelAndView.setViewName(LAYOUT_VIEW_NAME);
+
+        title = title == null ? DEFAULT_TITLE_NAME : title;
+        modelAndView.addObject(DEFAULT_THYMELEAF_TITLE_KEY, title);
+
+        return modelAndView;
+    }
+
+    ModelAndView redirect(String route) {
+        return this.view(this.DEFAULT_SPRING_REDIRECT_KW + route);
+    }
+}
