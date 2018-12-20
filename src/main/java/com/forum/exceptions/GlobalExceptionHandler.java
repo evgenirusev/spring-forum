5affad85af2ed7ebdaef43d359a72b52255e4a0d
rusev9 <evg.rusev@gmail.com>
2018-12-20 17:49:10 +0200

    Exception Handling

---
+++ src/main/java/com/forum/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.forum.exceptions;
+
+import com.forum.abstractions.controller.BaseController;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends BaseController {
+    private static final String DEFAULT_ERROR_MESSAGE = "There was an error with your request.";
+
+    @ExceptionHandler(RuntimeException.class)
+    public ModelAndView getException(RuntimeException e) {
+        String errorMessage = e.getClass().isAnnotationPresent(ResponseStatus.class)
+                ? e.getClass().getAnnotation(ResponseStatus.class).reason()
+                : DEFAULT_ERROR_MESSAGE;
+        return super.view("/views/exceptions/global", errorMessage, "Something Went Wrong");
+    }
+}
